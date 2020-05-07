af7b1f0fe1135e5d8b09fdd118a2aee75d19081d
Andre Ambrosio Boechat <boechat107@gmail.com>
2020-05-07 11:22:47 -0300

    Add an in-memory collection to hold journal pages

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::pages::Page;
+use crate::pages::Collection;
 use structopt::StructOpt;
 
 mod pages;
@@ -12,12 +12,20 @@ enum Journal {
     List,
 }
 
+fn exec_action(coll: &mut Collection, input: Journal) -> () {
+    match input {
+        Journal::New { text } => {
+            println!("New page id: {}", coll.add(text));
+            exec_action(coll, Journal::List)
+        }
+        Journal::List => println!("Jornal pages:\n{:?}", coll),
+    };
+}
+
 fn main() {
     let input_args = Journal::from_args();
     println!("Arguments: {:?}", input_args);
 
-    match input_args {
-        Journal::New { text } => println!("New page: {:?}", Page::new(12, text)),
-        _ => println!("Another command"),
-    }
+    let mut coll = Collection::new();
+    exec_action(&mut coll, input_args);
 }
--- src/pages.rs
+++ src/pages.rs
@@ -1,8 +1,8 @@
 use chrono::prelude::{Date, DateTime, Local};
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Page {
-    id: u32,
     text: String,
     date: Date<Local>,
     created_at: DateTime<Local>,
@@ -11,15 +11,37 @@ pub struct Page {
 }
 
 impl Page {
-    pub fn new(id: u32, text: String) -> Page {
+    pub fn new(text: String) -> Page {
         let now = Local::now();
         Page {
-            id,
             text,
             date: Local::today(),
             created_at: now,
             updated_at: now,
             tags: vec![],
         }
+    }
+}
+
+#[derive(Debug)]
+pub struct Collection {
+    pages: HashMap<u32, Page>,
+    id_cnt: u32,
+}
+
+impl Collection {
+    pub fn new() -> Collection {
+        Collection {
+            pages: HashMap::new(),
+            id_cnt: 0,
+        }
+    }
+    pub fn add(&mut self, text: String) -> u32 {
+        let id = self.id_cnt;
+
+        self.pages.insert(id, Page::new(text));
+        self.id_cnt += 1;
+
+        id
     }
 }
